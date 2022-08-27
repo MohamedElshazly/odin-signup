1c0a6b788d13eb2a4de010919785981bf3e74a83
omni <mohamedelshazly18@yahoo.com>
2022-08-27 20:29:03 +0200

    finished logo section

--- script.js
+++ script.js
@@ -7,10 +7,14 @@ pwd.addEventListener('input', () => {
     password = pwd.value;
     if(password == confirm_password){
         console.log("here")
-        document.documentElement.style.setProperty('--content-visibility', 'none')   
+        document.documentElement.style.setProperty('--content-visibility', 'none');
+        document.documentElement.style.setProperty('--indicator', '✓');
+        document.documentElement.style.setProperty('--indicatorColor', 'green');      
     }
     else{
-        document.documentElement.style.setProperty('--content-visibility', 'inline-block')   
+        document.documentElement.style.setProperty('--content-visibility', 'inline-block');
+        document.documentElement.style.setProperty('--indicator', '✖');
+        document.documentElement.style.setProperty('--indicatorColor', 'red');   
     }
 })
 
@@ -18,9 +22,13 @@ confirm_pwd.addEventListener('input', () => {
     confirm_password = confirm_pwd.value;
     if(password == confirm_password){
         console.log("here")
-        document.documentElement.style.setProperty('--content-visibility', 'none')   
+        document.documentElement.style.setProperty('--content-visibility', 'none');
+        document.documentElement.style.setProperty('--indicator', '✓');
+        document.documentElement.style.setProperty('--indicatorColor', 'green');   
     }
     else{
-        document.documentElement.style.setProperty('--content-visibility', 'inline-block')   
+        document.documentElement.style.setProperty('--content-visibility', 'inline-block');
+        document.documentElement.style.setProperty('--indicator', '✖');
+        document.documentElement.style.setProperty('--indicatorColor', 'red');   
     }
 })
